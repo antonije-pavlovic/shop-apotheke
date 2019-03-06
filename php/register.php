b5d3e613e3f2fda1c774580ca3c3617f6df1a2bb
Antonije Pavlovic <42003269+antonije-pavlovic@users.noreply.github.com>
2019-03-06 19:39:50 +0100

    Update register.php

--- php/register.php
+++ php/register.php
@@ -72,7 +72,7 @@ if($_SERVER['REQUEST_METHOD'] !== 'POST'){
                     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
                     $mail->SMTPAuth = true;                               // Enable SMTP authentication
                     $mail->Username = 'tokoricnickoimejezauzeto@gmail.com';                 // SMTP username
-                    $mail->Password = 'Lozinka1234!';                           // SMTP password
+                    $mail->Password = '^%$@$^%@^^&';                           // SMTP password
                     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
                     $mail->Port = 587;                                    // TCP port to connect to
 
